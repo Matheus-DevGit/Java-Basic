4d21b3fa439790f36815e595c1ad4733c5feff2b
Matheus Alves Batista <maa120120@gmail.com>
2021-05-26 16:28:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-alert("Hello World")
+// + Adição
+// - Subtração
+// * Multiplicação
+// / Divisão
+// % Módulo
+// ++ Incremento
+// -- Decremento
+
+var a= 30;
+var b= ++a;
+
+
+
+
+console.log(b);
