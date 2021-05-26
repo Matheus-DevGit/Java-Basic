2346827ababf1fad73777985acc3e53257d5cbe7
Matheus Alves Batista <maa120120@gmail.com>
2021-05-26 17:13:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,10 +6,18 @@
 // ++ Incremento
 // -- Decremento
 
-var a= 30;
-var b= ++a;
 
+var idade =  9;
 
+var maior65= idade >= 65;
+var menor10= idade <= 10;
 
+var gratuidade = menor10 || maior65;
 
-console.log(b);
+console.log("idade", idade);
+
+console.log(" Maior que 65 ", maior65);
+
+console.log(" Menor que 10 ", menor10);
+
+console.log("Tem direito a gratuidade ?", gratuidade)
